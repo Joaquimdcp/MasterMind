1313ec55ca5012a90a8f436a4800b6f413cd0ece
Joaquim <joaquimdcp@gmail.com>
2017-11-12 16:16:15 +0100

    Changes

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,24 @@ import java.util.Scanner;
 
 public class Main {
 
+    private static void login(){
+        boolean valid = false;
+        System.out.println("You choose login");
+        System.out.println("If you are already registered please press L to login or R to register");
+        valid = false;
+        Scanner s = new Scanner(System.in);
+        String c;
+        while(!valid){
+            c = s.nextLine();
+            if(c=="L" || c=="R") valid = true;
+            else System.out.println("Wrong: If you are already registered please press L to login or R to register");
+        }
+    }
+
+    private static void help(){
+        System.out.println("You choose help");
+    }
+
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         System.out.println("Welcome to MasterMind!");
@@ -18,16 +36,11 @@ public class Main {
         }
         switch (c){
             case "L":
-                System.out.println("You choose login");
-                System.out.println("If you are already registered please press L to login or R to register");
-                valid = false;
-                while(!valid){
-                    c = s.nextLine();
-                    if(c=="L" || c=="R") valid = true;
-                    else System.out.println("Wrong: If you are already registered please press L to login or R to register");
-                }
-
-
+                login();
+                break;
+            case "H":
+                help();
+                break;
         }
 
 
