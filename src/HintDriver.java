deed247d3573a70c430b0f4a58dcca65e4cc71c2
Joaquim <joaquimdcp@gmail.com>
2017-11-15 23:39:36 +0100

    Hint Driver #1

---
+++ src/HintDriver.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class HintDriver {
+    public static void main(String[] args){
+        /**
+         *  DRIVER
+         */
+        System.out.println("HINT DRIVER");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Method Options: ");
+        System.out.println("1: Constructor");
+        System.out.println("2: Get Hint");
+        int option = sc.nextInt();
+        switch (option){
+            case 1:
+                System.out.println("Constructor");
+            case 2:
+                System.out.println("Constructor");
+
+        }
+    }
+}
