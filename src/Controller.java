53cc5347d45c4804de68cc21d1c1e3e29e0575fc
Unknown <mariava.1213@gmail.com>
2017-11-16 23:25:27 +0100

    ControllerDriver done

--- src/Controller.java
+++ src/Controller.java
@@ -42,11 +42,11 @@ public class Controller {
         return this.currentGame;
     }
 
-    //public Ranking getCurrentRanking(){
+    public Ranking getCurrentRanking(){
         /** Getter of the current ranking
          */
-    //    return this.currentRanking;
-    //}
+        return this.currentRanking;
+    }
 
     public Round getRound(int n){
         /**Public function that returns an instance of the round number n (where n is the parameter of the function
@@ -70,6 +70,30 @@ public class Controller {
         this.currentGame.initGame(nColours, nPositions, nRounds, nHints);
     }
 
+    public String useHint() {
+        /** Public function that returns a hint (if the player has used less hints that it is permitted)
+         and increments the counter of number of hints
+         */
+        return this.currentGame.useHint();
+    }
+
+    public String playRound() {
+        /**Public function to play a round. It also controls if it's the last round (either because the game has been
+         * won or because it is the last trial)
+         */
+        if(!this.currentGame.playRound()){
+            this.currentGame.endGame();
+            if(this.currentGame.getWin()){
+                return "Game won";
+                updateRanking();////////////////////////////
+            }
+            else
+                return "Game ended but not won";
+        }
+        else
+            return "Round played normally";
+    }
+
     public boolean loadGame() {
         /**Public function to load a game
          */
@@ -82,41 +106,16 @@ public class Controller {
         return this.currentGame.saveGame();
     }
 
+    public boolean restartGame(){
+        /**Public function to restart a game
+         */
+        return this.currentGame.restartGame();
+    }
+
     public void exitGame() {
         /** Public function to exit a game: it will erase the current game
          */
         this.currentGame = new Game();
-    }
-
-    public void getRanking() {
-        /** Public function that returns the ranking
-         */
-
-        //FALTA FERLA!!!!!!!!!!!!!!!!!!!!
-    }
-
-    public String useHint() {
-        /** Public function that returns a hint (if the player has used less hints that it is permitted)
-        and increments the counter of number of hints
-        */
-        return this.currentGame.useHint();
-    }
-
-    public String playRound() {
-        /**Public function to play a round. It also controls if it's the last round (either because the game has been
-         * won or because it is the last trial)
-         */
-        if(this.currentGame.playRound()){
-            this.currentGame.endGame();
-            if(this.currentGame.getWin()){
-                return "Game won";
-                //ACTUALITZAR RANKING!!!!!!!!!!!!!!!!!!!
-            }
-            else
-                return "Game ended but not won";
-        }
-        else
-            return "Round played normally";
     }
 
     public boolean logIn(String name, String password) {
