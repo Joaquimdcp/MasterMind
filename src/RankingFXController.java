9113ce0d6fe7902766b6b4ba45f28ef689ee829f
Matias Sabato <matias.asabato@gmail.com>
2018-01-04 17:38:44 +0100

    Add files via upload

--- src/RankingFXController.java
+++ src/RankingFXController.java
@@ -12,56 +12,108 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.ResourceBundle;
+import java.util.StringTokenizer;
+import java.util.stream.Stream;
 
-public class RankingFXController {
+public class RankingFXController implements Initializable {
     @FXML
-    private Label user1, user2, user3, user4, user5, user6, user7, user8, user9, user10;
+    private Label user1 = new Label();
     @FXML
-    private Label score1, score2, score3, score4, score5, score6, score7, score8, score9, score10;
+    private Label user2 = new Label();
+    @FXML
+    private Label user3 = new Label();
+    @FXML
+    private Label user4 = new Label();
+    @FXML
+    private Label user5 = new Label();
+    @FXML
+    private Label user6 = new Label();
+    @FXML
+    private Label user7 = new Label();
+    @FXML
+    private Label user8 = new Label();
+    @FXML
+    private Label user9 = new Label();
+    @FXML
+    private Label user10 = new Label();
+    @FXML
+    private Label score1 = new Label();
+    @FXML
+    private Label score2 = new Label();
+    @FXML
+    private Label score3 = new Label();
+    @FXML
+    private Label score4 = new Label();
+    @FXML
+    private Label score5 = new Label();
+    @FXML
+    private Label score6 = new Label();
+    @FXML
+    private Label score7 = new Label();
+    @FXML
+    private Label score8 = new Label();
+    @FXML
+    private Label score9 = new Label();
+    @FXML
+    private Label score10 = new Label();
 
-    private LinkedHashMap<String, Integer> ranking;
+    private List<String> users = new ArrayList<String>() {{
+        add("----");
+        add("----");
+        add("----");
+        add("----");
+        add("----");
+        add("----");
+        add("----");
+        add("----");
+        add("----");
+        add("----");
+    }};
 
-    private List<Label> userLabels = new ArrayList<>();
-    private List<Label> scoreLabels = new ArrayList<>();
+    private List<String> scores = new ArrayList<String>() {{
+        add("----");
+        add("----");
+        add("----");
+        add("----");
+        add("----");
+        add("----");
+        add("----");
+        add("----");
+        add("----");
+        add("----");
+    }};
 
-    private List<String> users = new ArrayList<>();
-    private List<String> scores = new ArrayList<>();
-
-    private void fillLabelLists() {
-        userLabels.add(user1);
-        userLabels.add(user2);
-        userLabels.add(user3);
-        userLabels.add(user4);
-        userLabels.add(user5);
-        userLabels.add(user6);
-        userLabels.add(user7);
-        userLabels.add(user8);
-        userLabels.add(user9);
-        userLabels.add(user10);
-
-        scoreLabels.add(score1);
-        scoreLabels.add(score2);
-        scoreLabels.add(score3);
-        scoreLabels.add(score4);
-        scoreLabels.add(score5);
-        scoreLabels.add(score6);
-        scoreLabels.add(score7);
-        scoreLabels.add(score8);
-        scoreLabels.add(score9);
-        scoreLabels.add(score10);
-    }
-    /*
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        for (int i = 0; i < 10; i++) {
-            userLabels.get(i).setText(users.get(i));
-            scoreLabels.get(i).setText(scores.get(i));
-        }
-    }*/
+        updateLists();
+
+        user1.setText(users.get(0));
+        user2.setText(users.get(1));
+        user3.setText(users.get(2));
+        user4.setText(users.get(3));
+        user5.setText(users.get(4));
+        user6.setText(users.get(5));
+        user7.setText(users.get(6));
+        user8.setText(users.get(7));
+        user9.setText(users.get(8));
+        user10.setText(users.get(9));
+
+        score1.setText(scores.get(0));
+        score2.setText(scores.get(1));
+        score3.setText(scores.get(2));
+        score4.setText(scores.get(3));
+        score5.setText(scores.get(4));
+        score6.setText(scores.get(5));
+        score7.setText(scores.get(6));
+        score8.setText(scores.get(7));
+        score9.setText(scores.get(8));
+        score10.setText(scores.get(9));
+    }
 
     public void showMenu (ActionEvent event) throws IOException {
         Parent rankingParent = FXMLLoader.load(getClass().getResource("Menu.fxml"));
@@ -70,5 +122,21 @@ public class RankingFXController {
         Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
         window.setScene(rankingScene);
         window.show();
+    }
+
+    public void updateLists() {
+        try (Stream<String> stream = Files.lines(Paths.get("E:\\Mati\\Documents\\TI\\PROP\\IntelliJ\\GUI\\src\\sample\\ranking.txt"))) {
+            int i = 0;
+            for (String line : stream.toArray(String[]::new)) {
+                StringTokenizer rankingLine = new StringTokenizer(line, "#");
+                String username = rankingLine.nextToken();
+                String score = rankingLine.nextToken();
+                users.set(i, username);
+                scores.set(i, score);
+                i++;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
